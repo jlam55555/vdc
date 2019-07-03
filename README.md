3ba592555e6a18566bfa4ec0c3384b5e1906bcdc
Jonathan Lam <jlam55555@gmail.com>
2019-07-03 14:14:10 -0400

    added in bundling of g resources with glib-compile-resources

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ GTK+3 configuration utility for the [Veikk Linux Driver][1]
 
 **Still in alpha development.** Use the text-based tool in the driver repo for now. CLion used for development. Make sure to have `make` and `cmake` installed.
 
+Heavily based on the [GTK+ Building Applications tutorial][2].
+
 [1]: https://github.com/jlam55555/veikk-s640-driver
+[2]: https://developer.gnome.org/gtk3/stable/ch01s04.html
--- vdcappwin.c
+++ vdcappwin.c
@@ -8,12 +8,13 @@ struct _VDCAppWindow {
 
 G_DEFINE_TYPE(VDCAppWindow, vdc_app_window, GTK_TYPE_APPLICATION_WINDOW);
 
-static void vdc_app_window_init(VDCAppWindow *app) {
-    // pass
+static void vdc_app_window_init(VDCAppWindow *win) {
+    gtk_widget_init_template(GTK_WIDGET(win));
 }
 
 static void vdc_app_window_class_init(VDCAppWindowClass *class) {
-    // pass
+    gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(class),
+                                        "/com/github/jlam55555/vdc/window.ui");
 }
 
 VDCAppWindow *vdc_app_window_new(VDCApp *app) {
