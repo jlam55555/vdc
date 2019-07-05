b9d19926befb4f19cb82b1f5fdbe47638056f1e2
Jonathan Lam <jlam55555@gmail.com>
2019-07-05 15:28:27 -0400

    updated with combo box instead of radiobuttons

--- vdcapp.c
+++ vdcapp.c
@@ -10,6 +10,20 @@ void vdc_activate(GtkApplication *app, gpointer user_data) {
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
     gtk_builder_connect_signals(builder, NULL);
 
+
+    /** TODO: working here **/
+
+    GtkImage *image = GTK_IMAGE(gtk_image_new_from_file("image.jpeg"));
+    GdkPixbuf *pixbuf = gtk_image_get_pixbuf(image);
+
+    GObject *list_store = gtk_builder_get_object(builder, "orientation_list_store");
+
+    GtkTreeIter *tree_iter;
+//    gtk_tree_model_get_iter_first(GTK_TREE_MODEL(list_store), tree_iter);
+//    gtk_list_store_set(GTK_LIST_STORE(list_store), tree_iter, pixbuf);
+
+    /** TODO: end working here **/
+
     g_object_unref(builder);
 
     gtk_widget_show(window);
